561254da768e3fed63345bf7d572e02949824ab4
Matheus <101477339+matheusksn@users.noreply.github.com>
2023-08-17 21:50:15 -0300

    first commit

---
+++ src/main/java/com/ac/weblabapi/WebLabApiApplication.java
@@ -0,0 +1,13 @@
+package com.ac.weblabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLabApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ac/weblabapi/WebLabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.weblabapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
